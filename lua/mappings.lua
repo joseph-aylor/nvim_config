fc14091ee8f0ef2bc0b189eca0ae96e5fd83a2e4
Joe Aylor <joe@josephaylor.com>
2025-02-05 10:10:26 -0500

    close buffers in a sane way

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "qq", "<ESC>")
+map("n", "<leader>q",":bp<bar>sp<bar>bn<bar>bd<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
