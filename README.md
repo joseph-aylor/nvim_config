e77aa42fc0927db61c24c74477f1160050f3e012
Joe Aylor <joe@josephaylor.com>
2025-02-05 10:00:00 -0500

    fork from nvchad

---
+++ README.md
@@ -0,0 +1,16 @@
+** This repo is used as config by NvChad user **
+
+Updating my vim config. Things are so large I pulled it out of my dotfiles directory.
+
+I used nvchad as the base so I could get things off the ground quickly.
+
+**This repo is supposed to used as config by NvChad users!**
+
+- The main nvchad repo (NvChad/NvChad) is used as a plugin by this repo.
+- So you just import its modules , like `require "nvchad.options" , require "nvchad.mappings"`
+- So you can delete the .git from this repo ( when you clone it locally ) or fork it :)
+
+# Credits
+
+2) NVChad starter https://github.com/NvChad/NvChad as this starter. It made a lot of things faster!
+1) Lazyvim starter https://github.com/LazyVim/starter as nvchad's starter was inspired by Lazyvim's . It made a lot of things easier!
---
+++ init.lua
@@ -0,0 +1,52 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+vim.g.python_host_prog = "/home/joepa/venvs/nvim/venv/bin/python"
+vim.g.python3_host_prog = "/home/joepa/venvs/nvim/venv/bin/python"
+vim.g.loaded_python3_provider = "/home/joepa/venvs/nvim/venv/bin/python"
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+
+
+local enable_providers = {
+      "python3_provider",
+      "node_provider",
+      -- and so on
+    }
+    for _, plugin in pairs(enable_providers) do
+      vim.g["loaded_" .. plugin] = nil
+      vim.cmd("runtime " .. plugin)
+    end
